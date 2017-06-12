78852cf0b4b577ddeaa850a54d1478e1aba00bf2
Tom Flídr <tomflidr@gmail.com>
2017-06-12 23:10:17 +0200

    back in shape to get data from database, default getters by primary index attributes and by unique index attribute not implemented yet

--- Resource.vb
+++ Resource.vb
@@ -5,23 +5,23 @@ Public Class Resource
 
     Public Overrides Function GetTableColumns(connection As Databasic.Connection, table As String) As List(Of String)
         Dim result As New List(Of String)
-        Return Databasic.Statement.Prepare(
-                "SELECT c.COLUMN_NAME
+		Return Databasic.Statement.Prepare(
+				"SELECT c.COLUMN_NAME
                 FROM INFORMATION_SCHEMA.COLUMNS AS c
                 WHERE
                     c.TABLE_SCHEMA = @database AND
                     c.TABLE_NAME = @table
                 ORDER BY c.ORDINAL_POSITION",
-                connection
-            ).FetchAll(New With {
-                .database = connection.Provider.Database,
-                .table = table
-            }).ToList(Of String)()
-    End Function
+				connection
+			).FetchAll(New With {
+				.database = connection.Provider.Database,
+				.table = table
+			}).ToList(Of String)()
+	End Function
 
     Public Overrides Function GetLastInsertedId(transaction As Databasic.Transaction) As Object
-        Return Databasic.Statement.Prepare("SELECT LAST_INSERT_ID()", transaction).FetchOne().ToValue(Of Object)()
-    End Function
+		Return Databasic.Statement.Prepare("SELECT LAST_INSERT_ID()", transaction).FetchOne().ToInstance(Of Object)()
+	End Function
 
     Public Overrides Function GetAll(
             connection As Databasic.Connection,
@@ -38,7 +38,7 @@ Public Class Resource
             sql += If(orderByStatement.Length > 0, " ORDER BY " + orderByStatement, "") +
                     $" LIMIT {If(limit = 0, "18446744073709551615", limit.ToString())} OFFSET {offset}"
         End If
-        Return Statement.Prepare(sql, connection).FetchAll()
-    End Function
+		Return Databasic.Statement.Prepare(sql, connection).FetchAll()
+	End Function
 
 End Class
